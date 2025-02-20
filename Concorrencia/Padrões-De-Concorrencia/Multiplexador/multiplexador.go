d3b6b355ac2fc947d654ae26eac5e1413470d73d
Riquesdj <henriquesousadejesus2@hotmail.com>
2025-02-20 20:27:14 -0300

    Observações a mais

--- Concorrencia/Padrões-De-Concorrencia/Multiplexador/multiplexador.go
+++ Concorrencia/Padrões-De-Concorrencia/Multiplexador/multiplexador.go
@@ -15,12 +15,14 @@ func main() {
 
 }
 
+// função que vai receber dois canaias de e irá retornar somente 1
 func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
 	canalDeSaida := make(chan string)
 
 	go func() {
 		for {
 			select {
+			// caso o canal esteja pronto para passar o valor faça:
 			case mensagem := <-canalDeEntrada1:
 				canalDeSaida <- mensagem
 			case mensagem := <-canalDeEntrada2:
